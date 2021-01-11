9866699e8be88bb29aee24739e1c65b88fdedf8d
Denis Bulgakov <bulgakovd97@yandex.ru>
2021-01-12 02:46:31 +0300

    Проект Mesto исправленный на проверку

--- scripts/index.js
+++ scripts/index.js
@@ -1,44 +1,42 @@
-let editButton = document.querySelector('.profile__edit-button')
-let popup = document.querySelector('.popup')
-let closeButton = popup.querySelector('.popup__close-button')
-let nameInput = popup.querySelector('.popup__name')
-let occupationInput = popup.querySelector('.popup__occupation')
-let nameProfile = document.querySelector('.profile__name')
-let occupationProfile = document.querySelector('.profile__occupation')
+let editButton = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+let closeButton = popup.querySelector('.popup__close-button');
+let nameInput = popup.querySelector('.popup__input_type_name');
+let occupationInput = popup.querySelector('.popup__input_type_occupation');
+let nameProfile = document.querySelector('.profile__name');
+let occupationProfile = document.querySelector('.profile__occupation');
+let formElement = popup.querySelector('.popup__form');
 
 //Открытие попапа
 let openPopup = () => {
-    popup.classList.add('popup_opened')
+    popup.classList.add('popup_opened');
 
-    nameInput.value = nameProfile.textContent
-    occupationInput.value = occupationProfile.textContent
-}
-
-editButton.addEventListener('click', openPopup)
+    nameInput.value = nameProfile.textContent;
+    occupationInput.value = occupationProfile.textContent;
+};
 
 //Закрытие попапа
 let closePopup = () => {
-    popup.classList.remove('popup_opened')
-}
-
-closeButton.addEventListener('click', closePopup)
-
-popup.addEventListener('click', (event) => {
-    if (event.target === event.currentTarget) {
-        closePopup()
-    }
-})
+    popup.classList.remove('popup_opened');
+};
 
 //Сохранение данных о новом пользователе
-let formElement = popup.querySelector('.popup__form')
-
 let handleFormSubmit = (event) => {
     event.preventDefault();
 
-    nameProfile.textContent = nameInput.value
-    occupationProfile.textContent = occupationInput.value
+    nameProfile.textContent = nameInput.value;
+    occupationProfile.textContent = occupationInput.value;
 
-    closePopup()
-}
+    closePopup();
+};
 
-formElement.addEventListener('submit', handleFormSubmit)
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
+
+popup.addEventListener('mousedown', (event) => {
+    if (event.target === event.currentTarget) {
+        closePopup();
+    };
+});
+
+formElement.addEventListener('submit', handleFormSubmit);
