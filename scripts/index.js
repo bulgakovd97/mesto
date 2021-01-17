e3aa17fb26430c925e1a80bc2b3732678ce1f874
Denis Bulgakov <bulgakovd97@yandex.ru>
2021-01-18 01:07:27 +0300

    feat: сверстать попап добавления карточки

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,15 @@
 let editButton = document.querySelector('.profile__edit-button');
-let popup = document.querySelector('.popup');
-let closeButton = popup.querySelector('.popup__close-button');
-let nameInput = popup.querySelector('.popup__input_type_name');
-let occupationInput = popup.querySelector('.popup__input_type_occupation');
+let editPopup = document.querySelector('.edit-popup');
+let editCloseButton = editPopup.querySelector('.edit-popup__close-button');
+let nameInput = editPopup.querySelector('.edit-popup__input_type_name');
+let occupationInput = editPopup.querySelector('.edit-popup__input_type_occupation');
 let nameProfile = document.querySelector('.profile__name');
 let occupationProfile = document.querySelector('.profile__occupation');
-let formElement = popup.querySelector('.popup__form');
+let editFormElement = editPopup.querySelector('.edit-popup__form');
 
 //Открытие попапа
 let openPopup = () => {
-    popup.classList.add('popup_opened');
+    editPopup.classList.add('edit-popup_opened');
 
     nameInput.value = nameProfile.textContent;
     occupationInput.value = occupationProfile.textContent;
@@ -17,7 +17,7 @@ let openPopup = () => {
 
 //Закрытие попапа
 let closePopup = () => {
-    popup.classList.remove('popup_opened');
+    editPopup.classList.remove('edit-popup_opened');
 };
 
 //Сохранение данных о новом пользователе
@@ -31,12 +31,12 @@ let handleFormSubmit = (event) => {
 };
 
 editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
+editCloseButton.addEventListener('click', closePopup);
 
-popup.addEventListener('mousedown', (event) => {
+editPopup.addEventListener('mousedown', (event) => {
     if (event.target === event.currentTarget) {
         closePopup();
     };
 });
 
-formElement.addEventListener('submit', handleFormSubmit);
+editFormElement.addEventListener('submit', handleFormSubmit);
